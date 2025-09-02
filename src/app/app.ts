529d16e1d6690c980ac3e5126d533f26d128a8d6
f207765 <f207765@tjrn.jus.br>
2025-09-02 15:31:33 -0300

    feat: first commit.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoList } from './components/todo-list/todo-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/todo-list/todo-list.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [],
+  templateUrl: './todo-list.html',
+  styleUrl: './todo-list.css'
+})
+export class TodoList {
+
+  name: string = "Lista de Tarefas";
+
+}
